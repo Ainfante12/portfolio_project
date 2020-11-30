bd945f70caf3919a08da7c205cbae13765a1affe
Angeline <angelineinfante892@gmail.com>
2020-11-29 23:38:50 -0500

    pushing project to repo

---
+++ src/components/home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './CSS/home.css';
+
+
+
+
+function Home (){
+    return(
+        <div className="home">
+            <h1>Welcome To Angie's Page</h1>
+        
+        </div>
+    );
+
+}
+
+export default Home;
